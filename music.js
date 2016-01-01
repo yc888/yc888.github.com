d5a14437c6b8b85a0e4f54673dd008d447a76edc
yc888 <gavinliumanila@gmail.com>
2016-01-01 20:01:03 +0800

    修改加入监控提示

--- music.js
+++ music.js
@@ -1,7 +1,11 @@
 (function(){
   var music = $('<audio src="http://www.yuncai.la/images/pipayuls.mp3" loop="loop"></audio>')
   var ajaxs = '';
-  
+  var show = $('<div style="width:200px;text-align:center;height:50px;line-height:50px;background:#850000;color:#fff;position:absolute;top:0;right:0">监控刷新中···</div>')
+    
+  //监控提示
+  $('body').append(show);
+  //轮询开始
   setInterval(function(){
     if(ajaxs){
       ajaxs.abort();
