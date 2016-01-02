13f9b1ea01a690def7ac1185d9912103d75877c9
yc888 <gavinliumanila@gmail.com>
2016-01-02 10:04:28 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 (function(){
   var music = $('<audio src="http://www.yuncai.la/images/pipayuls.mp3" loop="loop"></audio>')
   var ajaxs = '';
-  var show = $('<div style="width:200px;text-align:center;height:50px;line-height:50px;background:#850000;color:#fff;position:absolute;top:0;right:0">监控刷新中···</div>')
+  var show = $('<div style="font-size:18px;width:300px;text-align:center;height:100px;line-height:100px;background:#850000;color:#fff;position:absolute;top:0;right:0">监控刷新中···</div>')
     
   //监控提示
   $('body').append(show);
@@ -21,6 +21,10 @@
        }else{
           music[0].play();
        }
+       show.text('监控刷新中···正常运行');
+     },
+     error: function(e){
+       show.text('监控刷新中···运行异常');
      }
     });
   }, 20000);
