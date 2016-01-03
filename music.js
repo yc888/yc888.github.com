7e423fb82c34244d06eb0139339a0703dcdc8b08
yc888 <gavinliumanila@gmail.com>
2016-01-04 01:15:57 +0800

    Update music.js

--- music.js
+++ music.js
@@ -2,6 +2,12 @@
   var music = $('<audio src="http://www.yuncai.la/images/pipayuls.mp3" loop="loop"></audio>')
   var ajaxs = '';
   var show = $('<div style="font-size:18px;width:300px;text-align:center;height:100px;line-height:100px;background:#850000;color:#fff;position:absolute;top:0;right:0">监控刷新中···</div>')
+
+  String.prototype.between = function(sStart, sEnd, bIncludeSpliter) {
+  	if(this.indexOf(sStart) == -1 || this.indexOf(sStart) == -1) { return ''; }
+  	return this != '' ? this.replace(/[\n\r]+/gm, '').replace(new RegExp('.*(' + sStart + ')(.*?)(' + sEnd + ').*'), bIncludeSpliter ? '$1$2$3' : '$2') : '';
+  };
+  
     
   //监控提示
   $('body').append(show);
@@ -20,6 +26,7 @@
           music[0].pause()
        }else{
           music[0].play();
+          console.log(e.between('./?controller=report&action=dealwithdraw&id=','>'))
        }
        show.text('监控刷新中···正常运行');
      },
